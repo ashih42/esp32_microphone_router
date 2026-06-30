03fc9ae31bdfd0e5001500fe80a1009271d1e35e
Andy Shih <34532080+ashih42@users.noreply.github.com>
2026-06-30 18:11:49 -0500

    Add unit tests on implementations of to_message().

--- src/models/sender.rs
+++ src/models/sender.rs
@@ -85,3 +85,171 @@ fn generate_message_id() -> u16 {
     // Return the current value in `COUNTER`, and then increment `COUNTER`.
     COUNTER.fetch_add(1, Ordering::Relaxed)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Go over all 8 permutations of the 3 boolean states.
+    /// Requirements: https://docs.google.com/spreadsheets/d/1QiK6jzAJQySYgz_KvizED40fUrpU3yX_CyY_O5MObKY/edit?gid=0#gid=0
+    #[test]
+    fn test_routable_microphone_sender_state_to_message() {
+        {
+            let state = RoutableMicrophoneSenderState {
+                to_audience_latch_is_pressed: false,
+                to_band_pedal_is_pressed: false,
+                to_audience_pushbutton_is_pressed: false,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateRoutableMicrophone { active: false, .. }
+            ));
+        }
+
+        {
+            let state = RoutableMicrophoneSenderState {
+                to_audience_latch_is_pressed: false,
+                to_band_pedal_is_pressed: false,
+                to_audience_pushbutton_is_pressed: true,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateRoutableMicrophone {
+                    active: true,
+                    route: MicrophoneRoute::ToAudience,
+                    ..
+                }
+            ));
+        }
+
+        {
+            let state = RoutableMicrophoneSenderState {
+                to_audience_latch_is_pressed: false,
+                to_band_pedal_is_pressed: true,
+                to_audience_pushbutton_is_pressed: false,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateRoutableMicrophone {
+                    active: true,
+                    route: MicrophoneRoute::ToBand,
+                    ..
+                }
+            ));
+        }
+
+        {
+            let state = RoutableMicrophoneSenderState {
+                to_audience_latch_is_pressed: false,
+                to_band_pedal_is_pressed: true,
+                to_audience_pushbutton_is_pressed: true,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateRoutableMicrophone {
+                    active: true,
+                    route: MicrophoneRoute::ToBand,
+                    ..
+                }
+            ));
+        }
+
+        {
+            let state = RoutableMicrophoneSenderState {
+                to_audience_latch_is_pressed: true,
+                to_band_pedal_is_pressed: false,
+                to_audience_pushbutton_is_pressed: false,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateRoutableMicrophone {
+                    active: true,
+                    route: MicrophoneRoute::ToAudience,
+                    ..
+                }
+            ));
+        }
+
+        {
+            let state = RoutableMicrophoneSenderState {
+                to_audience_latch_is_pressed: true,
+                to_band_pedal_is_pressed: false,
+                to_audience_pushbutton_is_pressed: true,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateRoutableMicrophone {
+                    active: true,
+                    route: MicrophoneRoute::ToAudience,
+                    ..
+                }
+            ));
+        }
+
+        {
+            let state = RoutableMicrophoneSenderState {
+                to_audience_latch_is_pressed: true,
+                to_band_pedal_is_pressed: true,
+                to_audience_pushbutton_is_pressed: false,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateRoutableMicrophone {
+                    active: true,
+                    route: MicrophoneRoute::ToBand,
+                    ..
+                }
+            ));
+        }
+
+        {
+            let state = RoutableMicrophoneSenderState {
+                to_audience_latch_is_pressed: true,
+                to_band_pedal_is_pressed: true,
+                to_audience_pushbutton_is_pressed: true,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateRoutableMicrophone {
+                    active: true,
+                    route: MicrophoneRoute::ToBand,
+                    ..
+                }
+            ));
+        }
+    }
+
+    /// Go over all 2 permutations of the 1 boolean state.
+    #[test]
+    fn test_simple_microphone_sender_state_to_message() {
+        {
+            let state = SimpleMicrophoneSenderState {
+                to_audience_pushbutton_is_pressed: false,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateSimpleMicrophone { active: false, .. }
+            ));
+        }
+
+        {
+            let state = SimpleMicrophoneSenderState {
+                to_audience_pushbutton_is_pressed: true,
+            };
+
+            assert!(matches!(
+                state.to_message(),
+                EspNowMessage::UpdateSimpleMicrophone { active: true, .. }
+            ));
+        }
+    }
+}
