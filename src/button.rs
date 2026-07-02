7e8ec87d502e861418026f4f786b14d0518802f7
Andy Shih <34532080+ashih42@users.noreply.github.com>
2026-07-01 22:58:01 -0500

    Refactor `Button`.

--- src/button.rs
+++ src/button.rs
@@ -1,13 +1,5 @@
-// use esp_hal::{
-//     // delay::Delay,
-//     gpio::{Input, InputConfig, InputPin, Level, Output, OutputConfig, Pull},
-//     time::{Duration, Instant},
-//     Config,
-// };
-
-use std::time::Instant;
-
 use esp_idf_hal::gpio::{Input, InputPin, PinDriver, Pull};
+use std::time::Instant;
 use timed_debouncer::Debouncer;
 
 /// This button sets up a GPIO input pin with internal pull-up resistor, to be used as a momentary pushbutton.
@@ -16,24 +8,23 @@ pub struct Button<'a> {
     debouncer: Debouncer<bool>,
     start_time: Instant,
     was_pressed: bool,
-    on_press_cb: Box<dyn FnMut()>,
-    on_release_cb: Box<dyn FnMut()>,
+    on_press_cb: Option<Box<dyn Fn()>>,
+    on_release_cb: Option<Box<dyn Fn()>>,
 }
 
 const DEFAULT_DEBOUNCE_TICKS: u32 = 20; // 20ms debounce delay
 
 impl<'a> Button<'a> {
-    pub fn new<P>(pin: P, on_press_cb: Box<dyn FnMut()>, on_release_cb: Box<dyn FnMut()>) -> Self
+    pub fn new<P>(
+        pin: P,
+        on_press_cb: Option<Box<dyn Fn()>>,
+        on_release_cb: Option<Box<dyn Fn()>>,
+    ) -> Self
     where
         P: InputPin + 'a,
     {
-        let pin_number = pin.pin();
-
-        let input = PinDriver::input(pin, Pull::Up)
-            .unwrap_or_else(|_| panic!("Could not set up button on pin {}", pin_number));
-
         Self {
-            input,
+            input: PinDriver::input(pin, Pull::Up).unwrap(),
             debouncer: Debouncer::new(),
             start_time: Instant::now(),
             was_pressed: false,
@@ -44,24 +35,33 @@ impl<'a> Button<'a> {
 
     /// Check if the button is in a pressed/released state with debouncer, and trigger callbacks on
     /// state-change events.
-    pub fn update(&mut self) {
+    fn update(&mut self) {
         let is_pressed = self.debouncer.update(
             self.input.is_low(),
             self.start_time.elapsed().as_millis() as u64,
             DEFAULT_DEBOUNCE_TICKS,
         );
 
-        if !self.was_pressed && is_pressed {
-            (self.on_press_cb)();
-        } else if self.was_pressed && !is_pressed {
-            (self.on_release_cb)();
+        if !self.was_pressed
+            && is_pressed
+            && let Some(callback) = &self.on_press_cb
+        {
+            (callback)();
+        } else if self.was_pressed
+            && !is_pressed
+            && let Some(callback) = &self.on_release_cb
+        {
+            (callback)();
         }
 
         self.was_pressed = is_pressed;
     }
 
-    pub async fn wait_and_update(&mut self) {
-        self.input.wait_for_any_edge().await.unwrap();
-        self.update();
+    /// Whenever a button is pressed or released, call its callback function.
+    pub async fn run(&mut self) {
+        loop {
+            self.input.wait_for_any_edge().await.unwrap();
+            self.update();
+        }
     }
 }
